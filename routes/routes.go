ae281742172bf62c93fd67da80d9c241d186dcd7
Ashish Rao <ashishrao2598@gmail.com>
2020-04-18 18:41:51 +0530

    add port as env

--- routes/routes.go
+++ routes/routes.go
@@ -3,10 +3,16 @@ package routes
 import (
 	"github.com/ashishra0/raspberry-pi-service/httpd/handler"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
 )
 
 func StartService() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	router := gin.Default()
 	router.GET("/", handler.PostToSmee)
 	router.POST("/", handler.HandlePost)
@@ -14,5 +20,5 @@ func StartService() {
 	router.NoRoute(func(context *gin.Context) {
 		context.AbortWithStatus(http.StatusNotFound)
 	})
-	router.Run(":8000")
+	router.Run(":" + port)
 }
