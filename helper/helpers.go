d2aa5d4730e2e92b4032a11e14f38a4bcdc0c382
Ashish Rao <ashishrao2598@gmail.com>
2020-04-20 00:13:07 +0530

    add comments

--- helper/helpers.go
+++ helper/helpers.go
@@ -5,23 +5,27 @@ import (
 	"time"
 )
 
-
+// GetTime is a helper function that returns the current time
+// In "DD-MM-YYYY H:M:S DAY" format.
 func GetTime() string {
 	dt := time.Now()
 
 	return dt.Format("01-02-2006 15:04:05 Monday")
 }
 
+// SetError is a helper function that assigns values to model.ErrorDB map.
 func SetError() {
 	model.ErrorDB["status"] = "No power"
 	model.ErrorDB["timestamp"] = GetTime()
 }
 
+// GetError() is a helper function that returns model.ErrorDB map.
 func GetError() map[string]string {
 	SetError()
 	return model.ErrorDB
 }
 
+// SetDefaultStatus() is a helper function that sets default values to model.MessageDB map after each GET request.
 func SetDefaultStatus() {
 	model.MessageDB["status"] = "No response from Tesla"
 	model.MessageDB["TimeStamp"] = GetTime()
