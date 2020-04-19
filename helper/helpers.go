2f2692c0d94c036e3b231565e17a3abc06418bcd
Ashish Rao <ashishrao2598@gmail.com>
2020-04-19 23:10:41 +0530

    initial commit

---
+++ helper/helpers.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"github.com/ashishra0/raspberry-pi-service/model"
+	"time"
+)
+
+
+func GetTime() string {
+	dt := time.Now()
+
+	return dt.Format("01-02-2006 15:04:05 Monday")
+}
+
+func SetError() {
+	model.ErrorDB["status"] = "No power"
+	model.ErrorDB["timestamp"] = GetTime()
+}
+
+func GetError() map[string]string {
+	return model.ErrorDB
+}
+
+func SetDefaultStatus() {
+	model.MessageDB["status"] = "No response from Tesla"
+	model.MessageDB["TimeStamp"] = GetTime()
+}
