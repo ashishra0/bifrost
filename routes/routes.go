3b7004e5da7cd5d01f099a3f8c4c0cb2fb9bde5c
Ashish Rao <ashishrao2598@gmail.com>
2020-04-21 01:26:52 +0530

    remove multiple service support

--- routes/routes.go
+++ routes/routes.go
@@ -6,8 +6,6 @@ import (
 	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
 var (
@@ -16,7 +14,7 @@ var (
 
 // StartService runs the gin application.
 // It is the single point of entry in the application.
-func StartTeslaService() http.Handler {
+func StartService() http.Handler {
 	// PORT for prod environment
 	port := "5000"
 	if port == "" {
@@ -29,65 +27,13 @@ func StartTeslaService() http.Handler {
 	router.POST("/api/power/status", handler.HandlePost)
 	// To return the saved response to user
 	router.GET("/api/power/status", handler.GetStatus)
+	// To get the data from hasura
+	router.GET("/api/expense", handler.GetExpense)
+	// To forward the event response from hasura
+	router.POST("/api/expense", handler.PostExpense)
 	// To handle the case of incorrect route
 	router.NoRoute(func(context *gin.Context) {
 		context.AbortWithStatus(http.StatusNotFound)
 	})
 	return router
-}
-
-
-func StartAlfredService() http.Handler {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-	router := gin.Default()
-	// To get the data from hasura
-	router.GET("/api/expense", handler.GetExpense)
-	// To forward the event response from hasura
-	router.POST("/api/expense", handler.PostExpense)
-	router.NoRoute(func(context *gin.Context) {
-		context.AbortWithStatus(http.StatusNotFound)
-	})
-	return router
-}
-
-func StartAllService() {
-	port01 := os.Getenv("PORT01")
-	port02 := os.Getenv("PORT02")
-
-	server01 := &http.Server{
-		Addr: ":" + port01,
-		Handler: StartTeslaService(),
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	server02 := &http.Server{
-		Addr: ":" + port02,
-		Handler: StartAlfredService(),
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	g.Go(func() error {
-		err := server01.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-		return err
-	})
-
-	g.Go(func() error {
-		err := server02.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-		return err
-	})
-
-	if err := g.Wait(); err != nil {
-		log.Fatal(err)
-	}
 }
