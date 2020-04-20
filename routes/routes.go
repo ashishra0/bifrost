ff2eb6c6cf620e84ad342df131a2001bcbcbf092
Ashish Rao <ashishrao2598@gmail.com>
2020-04-21 00:58:07 +0530

    add multiple services support

--- routes/routes.go
+++ routes/routes.go
@@ -3,14 +3,20 @@ package routes
 import (
 	"github.com/ashishra0/raspberry-pi-service/httpd/handler"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
 	"os"
+	"time"
+)
+
+var (
+	g errgroup.Group
 )
 
 // StartService runs the gin application.
 // It is the single point of entry in the application.
-func StartTeslaService() {
+func StartTeslaService() http.Handler {
 	// PORT for prod environment
 	port := "5000"
 	if port == "" {
@@ -27,11 +33,11 @@ func StartTeslaService() {
 	router.NoRoute(func(context *gin.Context) {
 		context.AbortWithStatus(http.StatusNotFound)
 	})
-	router.Run(":" + port)
+	return router
 }
 
 
-func StartAlfredService() {
+func StartAlfredService() http.Handler {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -44,5 +50,44 @@ func StartAlfredService() {
 	router.NoRoute(func(context *gin.Context) {
 		context.AbortWithStatus(http.StatusNotFound)
 	})
-	router.Run(":" + port)
+	return router
+}
+
+func StartAllService() {
+	port01 := os.Getenv("PORT01")
+	port02 := os.Getenv("PORT02")
+
+	server01 := &http.Server{
+		Addr: ":" + port01,
+		Handler: StartTeslaService(),
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server02 := &http.Server{
+		Addr: ":" + port02,
+		Handler: StartAlfredService(),
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	g.Go(func() error {
+		err := server01.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+		return err
+	})
+
+	g.Go(func() error {
+		err := server02.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+		return err
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
