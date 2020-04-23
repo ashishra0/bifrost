f16c7bb46a307ee85818d022546a108ecd96ec82
Ashish Rao <ashishrao2598@gmail.com>
2020-04-23 22:53:25 +0530

    define new func

---
+++ httpd/handler/createExpense.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/ashishra0/raspberry-pi-service/model"
+	"github.com/ashishra0/raspberry-pi-service/query"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+)
+
+// CreateExpense is handler function that will take the request body
+// and unmarshal it to model.New struct defined in model/expense
+// then It will call a mutation function and pass the unmarshaled struct
+// as argument.
+func CreateExpense(c *gin.Context) {
+	var record model.New
+	b, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(b, &record)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// @todo: Send this data to hasura DB
+	SendMutation(record)
+}
+
+// SaveVar will add new record in the []New and
+// return a map[string]interface{} of that record.
+func SaveVar (e *model.Expense, expense model.New) map[string]interface{} {
+	e.Expenses = append(e.Expenses, expense)
+	return map[string]interface{}{
+		"item_name": expense.ItemName,
+		"item_cost": expense.ItemCost,
+	}
+}
+
+// SendMutation saves the return type of SaveVar function
+// and save it as a variable and pass it to SendToHasura function
+// which is responsible for sending the actual mutation to the grapqhl
+// server.
+func SendMutation(v model.New) {
+	expenses := &model.Expense{}
+	variables := SaveVar(expenses, v)
+	query.SendToHasura(variables)
+}
