149fcc0290d8d6311b8b77ed67a4965d65ad7a16
Ashish Rao <ashishrao2598@gmail.com>
2020-04-25 01:51:30 +0530

    group routes

--- routes/routes.go
+++ routes/routes.go
@@ -16,19 +16,24 @@ func StartService() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
 	router := gin.Default()
-	// To trigger the webhook
-	router.GET("/api/power", handler.TriggerWebhook)
-	// To save the response to db
-	router.POST("/api/power/status", handler.HandlePost)
-	// To return the saved response to user
-	router.GET("/api/power/status", handler.GetStatus)
-	// To create a new record
-	router.POST("/api/expense/create", handler.CreateExpense)
-	// To get the data from hasura
-	router.GET("/api/expense", handler.GetExpense)
-	// To receive the event payload from hasura
-	router.POST("/api/expense", handler.FromHasura)
+
+	v1 := router.Group("/v1")
+	{
+		// To send a payload to smee.io
+		v1.GET("/power", handler.TriggerWebhook)
+		// To save the response to db
+		v1.POST("/power/status", handler.HandlePost)
+		// To return the saved response to user
+		v1.GET("/power/status", handler.GetStatus)
+		// To create a new expense record
+		v1.POST("/expense/create", handler.CreateExpense)
+		// To get the record from hasura
+		v1.GET("/expense", handler.GetExpense)
+		// To receive the event payload from hasura
+		v1.POST("/expense", handler.FromHasura)
+	}
 	// To handle the case of incorrect route
 	router.NoRoute(func(context *gin.Context) {
 		context.AbortWithStatus(http.StatusNotFound)
