eb18546506d0374970bf28b1dc0746d3cf8773ad
Ashish Rao <ashishrao2598@gmail.com>
2020-04-25 01:50:32 +0530

    add export comments

--- model/message.go
+++ model/message.go
@@ -1,10 +1,13 @@
 package model
 
+// Message defines the structure of the payload.
 type Message struct {
 	Status string `json:"status"`
 	Timestamp string `json:"timestamp"`
 }
 
+// MessageDB is a key value pair of the message payload.
 var MessageDB = make(map[string] string)
 
+// ErrorDB is a key value pair of the error payload.
 var ErrorDB = make(map[string] string)
