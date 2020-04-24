4445c6ffaabee401a10aa945931d8a9d18477a56
Ashish Rao <ashishrao2598@gmail.com>
2020-04-25 01:49:57 +0530

    initial commit

--- httpd/handler/getExpense.go
+++ httpd/handler/getExpense.go
@@ -1,7 +1,17 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/ashishra0/raspberry-pi-service/query"
+	"github.com/gin-gonic/gin"
+)
 
+// GetExpense calls the FetchFromHasura function and sends the
+// result returned from that function to the client.
 func GetExpense(c *gin.Context) {
-//	@todo
+	record := query.FetchFromHasura()
+	// @todo: Send record obtained from hasura to the client
+	for _, name := range record.Expenses {
+		fmt.Println(name)
+	}
 }
