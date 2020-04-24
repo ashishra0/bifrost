6be7d717f310d05d1b01f9716be92d46cc68edde
Ashish Rao <ashishrao2598@gmail.com>
2020-04-25 01:51:50 +0530

    add query

---
+++ query/fetchFromHasura.go
@@ -0,0 +1,23 @@
+package query
+
+import (
+	"github.com/ashishra0/raspberry-pi-service/model"
+	"github.com/shahidhk/gql"
+	"log"
+)
+
+// FetchFromHasua sends a query to hasura to fetch
+// all values from the db.
+func FetchFromHasura() model.Expense{
+	client := gql.NewClient("http://localhost:8080/v1/graphql", nil)
+	var record model.Expense
+	err := client.Execute(gql.Request{Query: `query {
+  Expense {
+    item_name
+  }
+}`}, &record)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return record
+}
